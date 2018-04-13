86557c64c81d23f693c4b4d5e46553f4b59a521b
zgeaw <32237384@qq.com>
2018-04-13 11:39:18 +0800

    updata

--- src/js/imageConfig.js
+++ src/js/imageConfig.js
@@ -2,5 +2,5 @@
  * Created by 32237384@qq.com on 2018/03/23.
  */
 
-//export default 'http://172.16.0.145:10000/qqface'
+//export default 'http://pics.sc.chinaz.com/Files/pic/faces/85/'
 export default 'images/qqface'
